fdd3742393e4c0c56f8f06399ef7fff1b3f2945b
Colton Milbrandt <coltonmilbrandt@gmail.com>
2022-08-29 11:49:11 -0600

    Update simulation.js
    Added comments

--- simulation.js
+++ simulation.js
@@ -7,22 +7,32 @@ var profitPercentages = [];
 var gainDifferentials = [];
 
 function myFunction() {
+  // Create new sheet if it doesn't exist already
   if (!ss.output) {
     var output = SpreadsheetApp.getActive().getSheetByName('Output');
   } else {
     var output = ss.insertSheet('Output');
   }
+
+  // Get tranch values from Simulation Sheet
   tranches.push(sheet.getRange('tranches').getValues());
   var length = tranches.length * 21;
   console.log(length);
   output.getRange(1,2,1,21).setValues(tranches);
+  // Count to determine cell ranges for when values are set on Output sheet
   count = 0;
+  
+  // For loop to run simulation i times
   for (var i = 1; i <= 50; i++) {
     refresh();
     // DO NOT change any values on the sheet or the data won't match !!!!!!
+    
+    // Retrieve values from Simulation Sheet
     profitPercentage = sheet.getRange('profitPercentage').getValue();
     gainDifferentials.push(sheet.getRange('gainDifferential').getValues());
     // profitPercentages.push([i],[profitPercentage],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""]);
+    
+    // push read profit percentage values to array
     profitPercentages.push([profitPercentage]);
     console.log(randomizer);
     console.log(profitPercentages);
@@ -34,6 +44,7 @@ function myFunction() {
   output.getRange(2,2,count,21).setValues(gainDifferentials);
 }
 
+// Changes a checkbox on the spreadsheet
 function refresh() {
   if(randomizer == false) {
     randomizer = true;
@@ -43,6 +54,7 @@ function refresh() {
   sheet.getRange('change').setValue(randomizer);
 }
 
+// Creates a range value
 function setRange(column, row) {
   var range = column + row;
   return range;
